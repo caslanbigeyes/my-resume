c7c73686a2a0703a28ea2e5f2cc49034bd7e7749
blue <53009552+caslanbigeyes@users.noreply.github.com>
2025-03-17 09:45:03 +0800

    add 优化

---
+++ src/app/Components/Layout.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const Layout = ({ children }) => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    return (
+        <div className="max-w-3xl mx-auto p-4 text-gray-900">
+            <header className="flex justify-between items-center py-4 border-b border-gray-300">
+                <h1 className="text-lg font-bold bg-black text-white px-4 py-2">个人记录</h1>
+                <nav className={`hidden md:flex space-x-4 text-gray-600`}>
+                    <Link href="/home" className="hover:text-black">首页</Link>
+                    <Link href="/about" className="hover:text-black">关于</Link>
+                    <Link href="/tags" className="hover:text-black">标签</Link>
+                    <Link href="/categories" className="hover:text-black">分类</Link>
+                </nav>
+                <button onClick={toggleNav} className="md:hidden text-gray-600">☰</button>
+            </header>
+            <nav className={`site-nav transition-transform transform border-x-sky-100 border-gray-300 bg-white shadow-md absolute top-19 left-0 right-0 z-10 p-4 rounded-lg duration-500 ease-in-out ${isNavOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-6 pointer-events-none'}`}>
+                <Link href="/home" className="block py-2 px-4 hover:bg-gray-200 transition-colors">首页</Link>
+                <Link href="/about" className="block py-2 px-4 hover:bg-gray-200 transition-colors">关于</Link>
+                <Link href="/tags" className="block py-2 px-4 hover:bg-gray-200 transition-colors">标签</Link>
+                <Link href="/categories" className="block py-2 px-4 hover:bg-gray-200 transition-colors">分类</Link>
+            </nav>
+            {children} {/* 渲染子组件 */}
+        </div>
+    );
+};
+
+export default Layout; 
--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,37 +1,13 @@
 'use client'
 import React, { useState } from "react";
 import Link from "next/link";
+import Layout from '../Components/Layout'
 
 export default function BlogPost() {
-    const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const toggleNav = () => {
-        if (isNavOpen) {
-            setIsNavOpen(false);
-        }
-        setIsNavOpen(!isNavOpen);
-    };
 
     return (
-        <div className="max-w-3xl mx-auto p-4 text-gray-900">
-            {/* 顶部导航 */}
-            <header className="flex justify-between items-center py-4 border-b border-gray-300">
-                <h1 className="text-lg font-bold bg-black text-white px-4 py-2">个人记录</h1>
-                <nav className={`hidden md:flex space-x-4 text-gray-600`}>
-                    <Link href="/home" className="hover:text-black">首页</Link>
-                    <Link href="/about" className="hover:text-black">关于</Link>
-                    <Link href="/tags" className="hover:text-black">标签</Link>
-                    <Link href="/categories" className="hover:text-black">分类</Link>
-                </nav>
-                <button onClick={toggleNav} className="md:hidden text-gray-600">☰</button>
-            </header>
-            <nav className={`site-nav transition-transform transform border-x-sky-100 border-gray-300 bg-white shadow-md absolute top-19 left-0 right-0 z-10 p-4 rounded-lg duration-500 ease-in-out ${isNavOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-6 pointer-events-none'}`}>
-                <Link href="/home" className="block py-2 px-4 hover:bg-gray-200 transition-colors">首页</Link>
-                <Link href="/about" className="block py-2 px-4 hover:bg-gray-200 transition-colors">关于</Link>
-                <Link href="/tags" className="block py-2 px-4 hover:bg-gray-200 transition-colors">标签</Link>
-                <Link href="/categories" className="block py-2 px-4 hover:bg-gray-200 transition-colors">分类</Link>
-            </nav>
-
+        <Layout>
             {/* 文章内容 */}
             <article className="mt-6" >
                 <h2 className="text-2xl font-bold">linux 有关网络操作的 11 个命令</h2>
@@ -52,6 +28,6 @@ export default function BlogPost() {
                 <h3 className="text-lg font-bold mt-6">curl & wget</h3>
                 <p className="mt-2 text-gray-700">curl 和 wget 都可以下载文件</p>
             </article>
-        </div>
+        </Layout>
     );
 }
